9a54333f166569a943d50e4401ec0bab29149ed4
Austin Amaral <austin.amaral3@gmail.comgit config --listgit config --global user.email austin.amaral3@gmail.com>
2023-07-28 13:23:00 -0700

    wip

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+
 
 // Suggested initial states
 const initialMessage = ''
@@ -14,41 +15,88 @@ const initialState = {
 }
 
 export default class AppClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  }
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
 
-  getXY = () => {
+  getXY = (index, gridWidth) => {
     // It it not necessary to have a state to track the coordinates.
     // It's enough to know what index the "B" is at, to be able to calculate them.
+    const x = index % gridWidth;
+    const y = Math.floor(index / gridWidth);
+    return { x, y };
   }
 
   getXYMessage = () => {
     // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
     // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
     // returns the fully constructed string.
+    const { index } = this.state;
+    const gridWidth = 3
+    const { x, y } = this.getXY(index, gridWidth);
+    return `Coordinates (${x}, ${y})`;
   }
 
   reset = () => {
     // Use this helper to reset all states to their initial values.
+    this.setState(initialState);
   }
 
   getNextIndex = (direction) => {
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
+    const { index } = this.state;
+    const gridWidth = 3
+    let nextIndex = index;
+
+    switch (direction) {
+      case 'left':
+        nextIndex = index - 1
+        break;
+      case 'up':
+        nextIndex = index - gridWidth
+        break;
+      case 'right':
+        nextIndex = index + 1
+        break;
+      case 'down': 
+        nextIndex = index + gridWidth
+        break;
+      default:
+        break;
+    }
+
+    if (nextIndex >= 0 && nextIndex < gridWidth * gridWidth) {
+      nextIndex;
+    } else {
+      return index;
+    }
   }
 
   move = (evt) => {
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
+    const direction = evt.target.id;
+    this.setState((prevState) => ({
+      index: this.getNextIndex(direction),
+      steps: prevState.steps + 1
+    }))
   }
 
   onChange = (evt) => {
     // You will need this to update the value of the input.
+    this.setState({ email: evt.target.value })
   }
 
   onSubmit = (evt) => {
     // Use a POST request to send a payload to the server.
+    evt.preventDefault();
+    const { email } = this.state;
+
   }
 
   render() {
@@ -69,17 +117,17 @@ export default class AppClass extends React.Component {
           }
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{this.state.message}</h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button id="left" onClick={this.move}>LEFT</button>
+          <button id="up" onClick={this.move}>UP</button>
+          <button id="right" onClick={this.move}>RIGHT</button>
+          <button id="down" onClick={this.move}>DOWN</button>
+          <button id="reset" onClick={this.reset}>reset</button>
         </div>
-        <form>
-          <input id="email" type="email" placeholder="type email"></input>
+        <form onSubmit={this.onSubmit}>
+          <input id="email" type="email" placeholder="type email" value={this.state.email} onChange={this.onChange}></input>
           <input id="submit" type="submit"></input>
         </form>
       </div>
