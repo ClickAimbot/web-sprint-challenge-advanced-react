866eeb5bd0bd8c386d8dfafeafe3367dee35a30a
Austin Amaral <austin.amaral3@gmail.comgit config --listgit config --global user.email austin.amaral3@gmail.com>
2023-08-15 23:46:41 -0700

    wip

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,6 +1,22 @@
-// Write your tests here
+import React from 'react';
+import { render } from '@testing-library/react';
+import AppClass from './AppClass';
+
+test('renders "Coordinates message', () => {
+  const { getByText } = render(<AppClass />);
+  const coordinatesMessage = getByText(/Coordinates/i);
+  expect(coordinatesMessage).toBeInTheDocument();
+})
 test('sanity', () => {
   expect(true).toBe(false)
 })
+test('square wont move past the edge of the board', () => {
+  const { container } = render(<AppClass />);
+  const square = container.querySelector('.square');
+  const currentIndex = Number(square.classList[1].split('-')[1])
+  const leftButton = container.querySelector('#left');
+  const updatedSquare = container.querySelector('.square');
+  const updatedIndex = Number(updatedSquare.classList[1].split('-')[1]);
 
-
+  expect(updatedIndex).toBe(currentIndex)
+})
