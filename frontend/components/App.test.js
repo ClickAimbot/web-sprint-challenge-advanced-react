0f7b8fc51d21ba4af544e6b52da9feec707b93a1
Austin Amaral <austin.amaral3@gmail.comgit config --listgit config --global user.email austin.amaral3@gmail.com>
2023-08-17 14:00:44 -0700

    done

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
-import useEvent from '@testing-library/user-event';
 import AppClass from './AppClass';
 
 test('renders without errors', () => {
   render(<AppClass />);
 });
+
+// test('renders the coordinates message header', () => {
+  // render(<AppClass />);
+ //  const header = screen.queryByText(/coordinates/i);
+  // expect(header).toBeInTheDocument();
+//});
